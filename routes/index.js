5e3fc3a25a68b1a069b1819b4faf91d8bac8cbe5
jumtech <despairing.clock+jumGithub@gmail.com>
2017-09-23 22:20:17 +0900

    get OAuth token

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const OAuth = require('oauth-1.0a');
+const crypto = require('crypto');
+const CONFIG = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const oauth = OAuth({
+    consumer: {
+      key: CONFIG.NOKIA_API_KEY,
+      secret: CONFIG.NOKIA_API_SECRET
+    },
+    signature_method: 'HMAC-SHA1',
+    hash_function: (base_string, key) => {
+      return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+    }
+  });
+  const data = {
+    url: 'https://developer.health.nokia.com/account/request_token',
+    method: 'GET'
+  };
+  console.log("oauth.authorize(data): "+JSON.stringify(oauth.authorize(data)));
+  request({
+    url: data.url,
+    method: data.method,
+    qs: oauth.authorize(data)
+  }, (err, res, body) => {
+    console.log("err: "+JSON.stringify(err));
+    console.log("res: "+JSON.stringify(res));
+    console.log("body: "+JSON.stringify(body));
+  });
   res.render('index', { title: 'Express' });
 });
 
